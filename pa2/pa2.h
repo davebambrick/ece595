da82e7c26812dce1ae3084a51a14daaccd7d77a6
Dave Bambrick <dmbambrick@gmail.com>
2021-10-27 16:25:09 -0400

    docstr; func headers for optimal packing

--- pa2/pa2.h
+++ pa2/pa2.h
@@ -21,7 +21,8 @@ struct NodeData {
 	string cutType;
 	vector<Pair2D> dimVec;
 };
-
+// Node data structure representing SBT construction
+// for block configuration
 struct BlockNode {
 	int label;
 	string cutType;
@@ -40,6 +41,10 @@ shared_ptr<BlockNode> buildTree(const string& inFileName);
 Pair2D computeFirstPackingDims(shared_ptr<BlockNode> node);
 void writePackingDims(Pair2D& pair, const string& fname);
 
-// Determine packing coordinates
+// Determine first-option packing coordinates and write to file
 void determinePacking(shared_ptr<BlockNode> root, const string& fname);
 void determinePackingUtil(shared_ptr<BlockNode> node, Pair2D& origin, FILE* outfile);
+
+// Determine optimal packing coordinates and write to file
+void determineOptimalPacking(shared_ptr<BlockNode> root, const string& fname);
+void determineOptimalPackingUtil(shared_ptr<BlockNode> node, Pair2D& origin, FILE* outfile);
